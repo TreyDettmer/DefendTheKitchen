a216a419463042223f9ed2abfa003f781ede1e08
TreyDettmer <dettmertrey@gmail.com>
2022-09-08 10:13:31 -0700

    ability to throw food, enemy takes damage

--- DefendTheKitchen/Enemy.gd
+++ DefendTheKitchen/Enemy.gd
@@ -8,6 +8,10 @@ export (NodePath) var playerNodePath;
 var player = null;
 var velocity = Vector2.ZERO;
 export var maxSpeed = 1.0;
+# since we aren't currently changing the sprite to make the enemy look fatter, we just change the size of the sprite
+export var damageSpriteScaleFactor = 1.25;
+var isDead = false;
+var healthPoints = 3;
 
 func _ready():
 	player = get_node(playerNodePath);
@@ -16,21 +20,41 @@ func _ready():
 
 
 func _process(delta):
+	if isDead:
+		return;
 	CalculateMovement(delta);
 		
 
-func CalculateMovement(delta):
+func CalculateMovement(_delta):
 	if player != null:
 		# move towards the player
 		navigationAgent.set_target_location(player.global_position);
 		var moveDirection = position.direction_to(navigationAgent.get_next_location());
-		velocity = moveDirection * maxSpeed * delta;
+		velocity = moveDirection * maxSpeed;
 		navigationAgent.set_velocity(velocity);
+		
+		
+		
+func takeDamage(damage):
+	if not isDead:
+		healthPoints -= damage;
+		# increase size of enemy
+		$AnimatedSprite.scale *= damageSpriteScaleFactor;
+		$CollisionShape2D.shape.radius *= damageSpriteScaleFactor;
+		if healthPoints <= 0:
+			die();
+func die():
+	if not isDead:
+		isDead = true;
+		queue_free();
 
 # signal called by set_velocity() method of navigationAgent
 func _on_NavigationAgent2D_velocity_computed(safe_velocity):
 	velocity = safe_velocity;
-	var _newVelocity = move_and_slide(velocity);
+	var _velocity = move_and_slide(velocity);
+	for index in get_slide_count():
+		var collision := get_slide_collision(index);
+		var body := collision.collider;
 
 # emit information needed to draw enemy's desired path
 func _on_NavigationAgent2D_path_changed():
--- DefendTheKitchen/Player.gd
+++ DefendTheKitchen/Player.gd
@@ -4,6 +4,12 @@ extends KinematicBody2D
 export var speed = 400.0;
 var screen_size = Vector2.ZERO;
 var direction = Vector2.ZERO;
+var aimDirection = Vector2.ZERO;
+var foods = {
+	"pizza" : preload("res://Pizza.tscn")
+}
+
+
 
 func _ready():
 	screen_size = get_viewport_rect().size;
@@ -22,10 +28,20 @@ func GetInput():
 		direction.y += 1;
 	if Input.is_action_pressed("move_up"):
 		direction.y -= 1;
+	aimDirection = (get_global_mouse_position() - global_position).normalized();
+	if Input.is_action_just_pressed("mouse_click"):
+		ThrowFood();
+		
 
-func _physics_process(delta):
-	var _newVelocity = move_and_slide(direction * speed * delta)
+func _physics_process(_delta):
+	var _newVelocity = move_and_slide(direction * speed)
 	# keep player within screen
 	position.x = clamp(position.x,0,screen_size.x);
 	position.y = clamp(position.y,0,screen_size.y);
 
+func ThrowFood():
+	var b = foods["pizza"].instance();
+	b.direction = aimDirection;
+	owner.add_child(b);
+	b.global_position = global_position;
+	
