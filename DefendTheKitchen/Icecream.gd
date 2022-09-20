e409ef0361dd9be208676b089d0a725deff332a6
TreyDettmer <dettmertrey@gmail.com>
2022-09-19 18:45:26 -0700

    fixed collision issue

--- DefendTheKitchen/Icecream.gd
+++ DefendTheKitchen/Icecream.gd
@@ -8,17 +8,8 @@ func _physics_process(_delta):
 	for index in get_slide_count():
 		var collision := get_slide_collision(index);
 		var body := collision.collider;
-		if body.is_in_group("enemies"):
-			HitEnemy(body);
 		destroy();
 		break;
-
-
-func _on_Area2D_area_entered(area):
-	if area.get_parent().is_in_group("enemies"):
-		HitEnemy(area.get_parent());
-	destroy();
-	
 
 func HitEnemy(enemy):
 	enemy.activateStatusEffect("frost")
